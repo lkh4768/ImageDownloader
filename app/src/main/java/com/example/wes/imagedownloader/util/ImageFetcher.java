a53e4c5bc8eac73a3916e38cc38f04818ce327b0
lkh4768 <lkh5510@gmail.com>
2016-02-28 11:19:47 +0900

    del decodeSampledBitmapFromStream method

--- app/src/main/java/com/example/wes/imagedownloader/util/ImageFetcher.java
+++ app/src/main/java/com/example/wes/imagedownloader/util/ImageFetcher.java
@@ -16,7 +16,7 @@ import java.io.InputStream;
 /**
  * TODO
  * if(MemCache size < picture size)
- *      diskCache.put(url, picture);
+ * diskCache.put(url, picture);
  */
 
 public class ImageFetcher {
@@ -60,65 +60,24 @@ public class ImageFetcher {
         @Override
         protected Bitmap doInBackground(String... params) {
             String url = params[0];
-            Bitmap bitmap = decodeSampledBitmapFromStream(url);
+            Bitmap bitmap = null;
 
-            if (bitmap != null)
-                imageCache.addBitmapToMemoryCache(url, bitmap);
+            try {
+                InputStream is = new java.net.URL(url).openStream();
+
+                bitmap = BitmapFactory.decodeStream(is);
+
+                if (bitmap != null)
+                    imageCache.addBitmapToMemoryCache(url, bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             return bitmap;
         }
 
         protected void onPostExecute(Bitmap bitmap) {
             imageView.setImageBitmap(bitmap);
-        }
-
-        private Bitmap decodeSampledBitmapFromStream(String url) {
-            InputStream is = null;
-            BitmapFactory.Options options = null;
-
-            try {
-                is = new java.net.URL(url).openStream();
-
-                // First decode with inJustDecodeBounds=true to check dimensions
-                options = new BitmapFactory.Options();
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeStream(is, null, options);
-                is.close();
-                // Calculate inSampleSize
-                options.inSampleSize = calculateInSampleSize(options, imageView.getWidth(), imageView.getHeight());
-
-                // Decode bitmap with inSampleSize set
-                options.inJustDecodeBounds = false;
-
-                is = new java.net.URL(url).openStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return BitmapFactory.decodeStream(is, null, options);
-        }
-
-        private int calculateInSampleSize(
-                BitmapFactory.Options options, int reqWidth, int reqHeight) {
-            // Raw height and width of image
-            final int height = options.outHeight;
-            final int width = options.outWidth;
-            int inSampleSize = 1;
-
-            if (height > reqHeight || width > reqWidth) {
-
-                final int halfHeight = height / 2;
-                final int halfWidth = width / 2;
-
-                // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-                // height and width larger than the requested height and width.
-                while ((halfHeight / inSampleSize) > reqHeight
-                        && (halfWidth / inSampleSize) > reqWidth) {
-                    inSampleSize *= 2;
-                }
-            }
-
-            return inSampleSize;
         }
     }
 
